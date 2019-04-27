764a24534d6faa8a619b735f8aa9ec5c3c1c01ae
ArtemKOrzh <49811596+ArtemKOrzh@users.noreply.github.com>
2019-04-27 18:04:46 +0500

    Create ser1.py

---
+++ ser1.py
@@ -0,0 +1,20 @@
+from flask import flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api/save-image/')
+def add_data_handler():
+	if request.form.get('secret'):
+		category = request.form.get('category')
+		image = request.files.get('image')
+		...
+		return jsonify(dict(success=1))
+
+	return jsonify(dict(success=0))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
