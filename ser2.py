3704a2fe63f42d631d6fb981ef9b00d47c6bcd24
ArtemKOrzh <49811596+ArtemKOrzh@users.noreply.github.com>
2019-04-27 18:05:33 +0500

    Create ser2.py

---
+++ ser2.py
@@ -0,0 +1,22 @@
+from flask import flask
+from flask import request
+from flask import render_file
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_file('index.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+	category = request.form.get('category')
+	image = request.files.get('image')
+	...
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
